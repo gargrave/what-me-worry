eedf03f15b96d9626eab1e39152d32bfb4996d53
Gabe Hargrave <gabe.hargrave@vacasa.com>
2019-04-14 21:04:49 -0700

    Update functional checkout

---
+++ src/codekata/09-Checkout/rules.ts
@@ -0,0 +1,18 @@
+export type PricingRules = {
+  [key: string]: {
+    [key: number]: number
+  }
+}
+
+export const defaultPricingRules: PricingRules = {
+  A: {
+    1: 50,
+    3: 130,
+  },
+  B: {
+    1: 30,
+    2: 45,
+  },
+  C: { 1: 20 },
+  D: { 1: 15 },
+}
